955338a645a49f96171040d200191a68e3381675
Diego Espindola <diegoespindola9404@gmail.com>
2025-07-16 21:24:52 -0300

    feat(schemas): Adicionar classe subscription aos schemas

--- app/schemas.py
+++ app/schemas.py
@@ -1,6 +1,22 @@
 from pydantic import BaseModel, HttpUrl
 from datetime import datetime
+from typing import List
+from enum import Enum
 
+
+# Subscription
+class TagEnum(str, Enum):
+    bug_fix = "bug_fix"
+    update = "update"
+    deprecate = "deprecate"
+    new_feature = "new_feature"
+    security_fix = "security_fix"
+
+class Subscription(BaseModel):
+    tags: List[TagEnum]
+    libraries_list: List[str]
+
+# News
 class News(BaseModel):
     description: str
     tag: str
