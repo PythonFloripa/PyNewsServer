06b000c6853e92852d197708434cce8513e23769
Diego Espindola <diegoespindola9404@gmail.com>
2025-07-18 22:26:13 -0300

    chore(database): fake user class

--- app/services/database/database.py
+++ app/services/database/database.py
@@ -1,14 +1,14 @@
 # app/services/database.py
 
-from app.schemas import UserInDB
+from app.schemas import CommunityInDB
 
 # Simulando um banco com hash de senha
 from app.services.auth import get_password_hash
 
 fake_users_db = {
-    "alice": UserInDB(
+    "alice": CommunityInDB(
         username="alice",
-        full_name="Alice Liddell",
+        full_name="Alice in the Maravilha's world",
         email="alice@example.com",
         password=get_password_hash("secret123")
     )
