2c80deb2d87783680806924052215e331a36d151
Diego Espindola <diegoespindola9404@gmail.com>
2025-07-25 21:48:51 -0300

    feat(db): estrutura inicial do banco com modelo Community
    Definição inicial do modelo Community. Substituição da antiga modelagem para organização em community_model.py.

--- app/services/database/community.py
+++ app/services/database/community.py
@@ -1,12 +1,12 @@
 # app/services/database/community.py
 from sqlmodel import select
 from sqlalchemy.exc import NoResultFound
-from app.schemas import Community
+from app.services.database.model.community_model import Community
 from app.services.database.database import get_session
 
 async def get_community_by_username(username: str):
     async for session in get_session():
         stmt = select(Community).where(Community.username == username)
         result = await session.exec(stmt)
-        user = result.scalar_one_or_none()
+        user = result.one_or_none()
         return user
