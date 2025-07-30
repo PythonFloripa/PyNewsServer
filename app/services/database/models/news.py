16632f4ff6c83148db9cb3c28f1c7739aa6b21a4
IkeSalmonson <lhfsalmonson@gmail.com>
2025-07-30 12:17:47 -0300

    Refactor News model for improved readability and organization

--- app/services/database/models/news.py
+++ app/services/database/models/news.py
@@ -1,26 +1,33 @@
-from typing import Optional
 from datetime import datetime
-from sqlmodel import SQLModel, Field
+from typing import Optional
+
+from sqlmodel import Field, SQLModel
+
 
 class News(SQLModel, table=True):
     __tablename__ = "news"
 
     # Campos obrigatórios e suas definições
     id: Optional[int] = Field(default=None, primary_key=True)
-    title: str # = Field(index=True) # Indexado para buscas mais rápidas por título
+    title: str
     content: str
-    author: str # Informação esta no endpoint e não aparece no schema do db
+    author: str
     category: str
     user_email: str
     source_url: str
-    tags: str # Poderia ser uma lista de strings em um modelo mais complexo, mas por simplicidade mantemos como string
+    tags: str
     social_media_url: str
-    likes: int = Field(default=0) # Valor padrão de 0 curtidas
+    likes: int = Field(default=0)
 
     # Chaves estrangeiras
-    community_id: Optional[int] = Field(default=None, foreign_key="communities.id")
-    # library_id: Optional[int] = Field(default=None, foreign_key="libraries.id")
+    community_id: Optional[int] = Field(
+            default=None,
+            foreign_key="communities.id")
+    # library_id: Optional[int]=Field(default=None, foreign_key="libraries.id")
 
     # Campos de data/hora
-    created_at: Optional[datetime] = Field(default_factory=datetime.now) # Preenchido automaticamente na criação
-    updated_at: Optional[datetime] = Field(default_factory=datetime.now, sa_column_kwargs={"onupdate": datetime.now}) # Atualizado automaticamente na modificação
+    created_at: Optional[datetime] = Field(default_factory=datetime.now)
+    updated_at: Optional[datetime] = Field(
+        default_factory=datetime.now,
+        sa_column_kwargs={"onupdate": datetime.now}
+    )
