f0655b86c4d1326448155c4aa2d56ea008322173
Gildo Junior <1gildojunior@gmail.com>
2025-09-01 21:22:35 -0300

    test: update library and subscription tests for consistency and add new API tests

--- tests/test_subscriptions.py
+++ tests/test_subscriptions.py
@@ -1,10 +1,10 @@
 import pytest
 import pytest_asyncio
-
+from httpx import AsyncClient
+from schemas import SubscriptionTagEnum
+from services.database.models import Community, Subscription
 from sqlmodel import select
 from sqlmodel.ext.asyncio.session import AsyncSession
-
-from services.database.models import Community, Subscription
 
 
 @pytest_asyncio.fixture
@@ -20,17 +20,83 @@ async def community(session: AsyncSession):
 async def test_insert_subscription(session: AsyncSession, community: Community):
     subscription = Subscription(
         email="teste@teste.com",
-        tags="teste,Python",
+        tags=[SubscriptionTagEnum.BUG_FIX, SubscriptionTagEnum.UPDATE],
         community_id=community.id,
     )
     session.add(subscription)
     await session.commit()
 
-    statement = select(Subscription).where(Subscription.email == "teste@teste.com")
+    statement = select(Subscription).where(
+        Subscription.email == "teste@teste.com"
+    )
     result = await session.exec(statement)
     found = result.first()
 
     assert found is not None
     assert found.email == "teste@teste.com"
-    assert found.tags == "teste,Python"
+    assert found.tags == [
+        SubscriptionTagEnum.BUG_FIX,
+        SubscriptionTagEnum.UPDATE,
+    ]
     assert found.community_id == community.id
+
+
+@pytest_asyncio.fixture
+async def preset_library(async_client: AsyncClient):
+    body1 = {
+        "library_name": "Python",
+        "releases_url": "http://teste.com/",
+        "logo": "http://teste.com/",
+    }
+
+    response1 = await async_client.post(
+        "/api/libraries",
+        json=body1,
+        headers={"Content-Type": "application/json"},
+    )
+
+    assert response1.status_code == 200
+
+    body2 = {
+        "library_name": "Django",
+        "releases_url": "http://teste.com/",
+        "logo": "http://teste.com/",
+    }
+
+    response2 = await async_client.post(
+        "/api/libraries",
+        json=body2,
+        headers={"Content-Type": "application/json"},
+    )
+
+    assert response2.status_code == 200
+
+
+@pytest.mark.asyncio
+async def test_post_subscribe_endpoint(
+    async_client: AsyncClient, session: AsyncSession
+):
+    body = {
+        "email": "teste@teste.com",
+        "tags": ["bug_fix", "update"],
+        "libraries_list": ["Python", "Django"],
+    }
+
+    response = await async_client.post(
+        "/api/libraries/subscribe",
+        json=body,
+        headers={"Content-Type": "application/json"},
+    )
+
+    assert response.status_code == 200
+    assert response.json()["status"] == "Subscribed in libraries successfully"
+
+    statement = select(Subscription).where(Subscription.email == body["email"])
+    result = await session.exec(statement)
+    created_subscription = result.first()
+
+    assert created_subscription is not None
+    assert created_subscription.tags == [
+        SubscriptionTagEnum.BUG_FIX,
+        SubscriptionTagEnum.UPDATE,
+    ]
